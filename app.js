e65a891247a587348ea2554e36c03a61ca6da64f
Stanley Louis <stanlouis@msn.com>
2018-05-25 21:17:37 -0400

    Set up addTask

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+$ (function () {
+  // Define UI Vars
+  const $form = $ ('#task-form');
+  const $taskList = $ ('.collection');
+  const $clearBtn = $ ('.clear-tasks');
+  const $filter = $ ('#filter');
+  const $taskInput = $ ('#task');
 
+  // Load all event listeners
+  loadEventListeners();
+
+  function loadEventListeners () {
+    $form.submit (addTask);
+  }
+
+  function addTask (e) {
+    if ($taskInput.val () === '') {
+      alert ('Add a task');
+    }
+    // Create li element
+    const $li = $ ('<li />');
+    // Add class and textNode
+    $li.addClass ('collection-item').text ($taskInput.val ());
+    // Crete link
+    const $link = $ ('<a></a>');
+    // Add class and icon
+    $link
+      .addClass ('delete-item secondary-content')
+      .html ('<i class="fas fa-times"></i>');
+    // Append link to li
+    $li.append ($link);
+    // Append li to ul
+    $taskList.append ($li);
+    // Clear text input
+    $taskInput.val ('');
+    e.preventDefault ();
+  }
+});
